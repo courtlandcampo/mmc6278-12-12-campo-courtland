9cad8c4cfad3233752e0e51d97c288201275a540
courtlandcampo <courtland.campo@ufl.edu>
2022-08-07 15:45:51 -0400

    getWinOrLoss function added

--- index.js
+++ index.js
@@ -70,7 +70,15 @@ class Word {
   }
 
   // implement the getWinOrLoss function:
-  getWinOrLoss() {}
+  getWinOrLoss() {
+    if (this.word === this.displayWord && this.remainingGuesses > 0){
+      return 'win'
+    } else if (this.word != this.displayWord && this.remainingGuesses <= 0){
+      return 'loss'
+    } else {
+      return null
+    }
+  }
 }
 
 function newGame() {
